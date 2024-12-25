948134a1b3605338ef10941a4447f65457f283b4
KenNuYu <tanjy0206@gmail.com>
2024-12-25 22:44:25 +0800

    feat: product class updated

--- server/src/Models/Product.cs
+++ server/src/Models/Product.cs
@@ -6,17 +6,20 @@ namespace vegeatery.Models
 {
     public class Product
     {
-        [Required]
-        public int productId { get; set; }
+        public int ProductId { get; set; }
 
         [Required, MinLength(3), MaxLength(50)]
-        public string productName { get; set; }
+        public string ProductName { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ProductPrice { get; set; }
 
         [Required, MinLength(3), MaxLength(500)]
-        public string productDescription { get; set; }
+        public string ProductDescription { get; set; }
 
         [Required, MinLength(3), MaxLength(50)]
-        public string productCategory { get; set; }
+        public string ProductCategory { get; set; }
 
         [Column(TypeName = "datetime")]
         public DateTime CreatedAt { get; set; }
