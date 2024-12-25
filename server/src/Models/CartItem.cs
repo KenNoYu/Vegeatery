27f48a0d66dbeac4f73c41ad0e2add6b25b86f61
KenNuYu <tanjy0206@gmail.com>
2024-12-25 22:43:21 +0800

    feat: cart item class

---
+++ server/src/Models/CartItem.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace vegeatery.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+
+        [Required, Range(1, 10000)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required, Range(1, 10)]
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime UpdatedAt { get; set; }
+
+        // Foreign Key
+        [Required]
+        public int CartId { get; set; } // Foreign key to Cart
+
+        [Required]
+        public int ProductId { get; set; } // Foreign key to Product
+
+        // Navigation properties
+        public Cart? Cart { get; set; }
+        public Product? Product { get; set; }
+    }
+}
