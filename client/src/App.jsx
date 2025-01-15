2322ee06b0c2ff0289f3b3df80694eb1c5c982b5
charlotte575757 <charlottepny@gmail.com>
2025-01-15 17:37:58 +0800

    added in the template logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -41,6 +41,8 @@ import Login from "./pages/Login";
 function App() {
   const [user, setUser] = useState(null);
 
+
+
   useEffect(() => {
     if (localStorage.getItem("accessToken")) {
       http.get("/user/auth").then((res) => {
@@ -49,53 +51,58 @@ function App() {
     }
   }, []);
 
+  const logout = () => {
+    localStorage.clear();
+    window.location = "/";
+  };
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
-  <Router>
-    <ThemeProvider theme={MyTheme}>
-      <AppBar position="static" className="AppBar">
-        <Container>
-          <Toolbar disableGutters={true} sx={{ width: '100%', justifyContent: 'space-between' }}>
-            
-            {/* Left side: 3 directories */}
-            <Box sx={{ display: 'flex', gap: 3 }}>
-              <Link to="/store"><Typography>Store</Typography></Link>
-              <Link to="/rewards"><Typography>Rewards</Typography></Link>
-              <Link to="/reserve"><Typography>Reserve</Typography></Link>
-            </Box>
+      <Router>
+        <ThemeProvider theme={MyTheme}>
+          <AppBar position="static" className="AppBar">
+            <Container>
+              <Toolbar disableGutters={true} sx={{ width: '100%', justifyContent: 'space-between' }}>
 
-            {/* Center logo (now an image) */}
-            <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-              <Link to="/home">
-                <img 
-                  src={logo} 
-                  alt="Vegeatery Logo" 
-                  style={{ height: '50px', width: 'auto' }} 
-                />
-              </Link>
-            </Box>
+                {/* Left side: 3 directories */}
+                <Box sx={{ display: 'flex', gap: 3 }}>
+                  <Link to="/store"><Typography>Store</Typography></Link>
+                  <Link to="/rewards"><Typography>Rewards</Typography></Link>
+                  <Link to="/reserve"><Typography>Reserve</Typography></Link>
+                </Box>
 
-            {/* Right side: 2 directories and sign-in button */}
-            <Box sx={{ display: 'flex', gap: 3 }}>
-              <Link to="/story"><Typography>Our Story</Typography></Link>
-              <Link to="/feedback"><Typography>Feedback</Typography></Link>
-              
-              {user ? (
-                <>
-                  <Typography>{user.name}</Typography>
-                  <Button onClick={logout} color="Accent">Logout</Button>
-                </>
-              ) : (
-                <>
-                  <Link to="/register"><Typography color="Accent"><b>Register</b></Typography></Link>
-                  <Link to="/login"><Typography color="Accent"><b>Login</b></Typography></Link>
-                </>
-              )}
-            </Box>
+                {/* Center logo (now an image) */}
+                <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+                  <Link to="/home">
+                    <img
+                      src={logo}
+                      alt="Vegeatery Logo"
+                      style={{ height: '50px', width: 'auto' }}
+                    />
+                  </Link>
+                </Box>
 
-          </Toolbar>
-        </Container>
-      </AppBar>
+                {/* Right side: 2 directories and sign-in button */}
+                <Box sx={{ display: 'flex', gap: 3 }}>
+                  <Link to="/story"><Typography>Our Story</Typography></Link>
+                  <Link to="/feedback"><Typography>Feedback</Typography></Link>
+
+                  {user ? (
+                    <>
+                      <Typography>{user.name}</Typography>
+                      <Button onClick={logout} color="Accent">Logout</Button>
+                    </>
+                  ) : (
+                    <>
+                      <Link to="/register"><Typography color="Accent"><b>Register</b></Typography></Link>
+                      <Link to="/login"><Typography color="Accent"><b>Login</b></Typography></Link>
+                    </>
+                  )}
+                </Box>
+
+              </Toolbar>
+            </Container>
+          </AppBar>
 
           <Container>
             <Routes>
@@ -103,23 +110,23 @@ function App() {
               <Route path="/rewards/user/pointshistory" element={<PointsHistory />} />
               <Route path="/rewards/admin/voucherssystem" element={<AdminVouchersSystem />} />
               <Route path="/rewards/admin/pointsrange" element={<PointsRange />} />
-              <Route path="/feedback/user/generalfeedback" element={<GeneralFeedback/>} />
-              <Route path="/general-feedback/edit/:id" element={<GeneralFeedbackEdit/>} />
-              <Route path="/feedback/admin/generalfeedback" element={<AdminGeneralFeedback/>} />
-          {/* RESERVATION */}
-          <Route path="/reserve" element={<ReservationPage/>} />
-          <Route path="/reserve/confirmed" element={<ConfirmationPage/>} />
-          <Route path="/staff/viewreservations" element={<StaffReservations/>} />
-          <Route path="/staff/reservationlogs" element={<StaffReserveLogs/>} />
-          <Route path="/staff/viewreservations/:id" element={<StaffFocusedReservation/>}/>
+              <Route path="/feedback/user/generalfeedback" element={<GeneralFeedback />} />
+              <Route path="/general-feedback/edit/:id" element={<GeneralFeedbackEdit />} />
+              <Route path="/feedback/admin/generalfeedback" element={<AdminGeneralFeedback />} />
+              {/* RESERVATION */}
+              <Route path="/reserve" element={<ReservationPage />} />
+              <Route path="/reserve/confirmed" element={<ConfirmationPage />} />
+              <Route path="/staff/viewreservations" element={<StaffReservations />} />
+              <Route path="/staff/reservationlogs" element={<StaffReserveLogs />} />
+              <Route path="/staff/viewreservations/:id" element={<StaffFocusedReservation />} />
 
-          <Route path="/" element={<Tutorials />} />
-          <Route path="/tutorials" element={<Tutorials />} />
-          <Route path="/addtutorial" element={<AddTutorial />} />
-          <Route path="/edittutorial/:id" element={<EditTutorial />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
- 
+              <Route path="/" element={<Tutorials />} />
+              <Route path="/tutorials" element={<Tutorials />} />
+              <Route path="/addtutorial" element={<AddTutorial />} />
+              <Route path="/edittutorial/:id" element={<EditTutorial />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+
 
             </Routes>
           </Container>
