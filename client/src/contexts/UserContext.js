9b091497e334eb884dea1c3fefee8e9448f8a7db
AndrewAce <112236518+AndrewAce@users.noreply.github.com>
2025-01-16 12:18:10 +0800

    feat: updated UserContext for user info

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,8 +1,25 @@
-import { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import http from '../http'; // Assuming you have an HTTP client
 
-const UserContext = createContext({
-  user: null,
-  setUser: () => {}
-});
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      http
+        .get("/Auth/auth")
+        .then((res) => {
+          setUserId(res.data.user.id); 
+        })
+        .catch((err) => {
+          console.error("Error fetching user data", err);
+        });
+    }
+  }, []);
+
+  return userId; // Return the userId as an object
+};
 
 export default UserContext;
