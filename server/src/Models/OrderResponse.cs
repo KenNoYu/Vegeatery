c6c4995eb1e051a04ccdebf67cafb830b0ff65ed
KenNoYu <tanjy0206@gmail.com>
2025-01-17 15:00:12 +0800

    feat: added response and request DTOs

---
+++ server/src/Models/OrderResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace vegeatery.Models
+{
+	public class OrderResponse
+	{
+		public int OrderId { get; set; }
+		public string FullName { get; set; }
+		public string Email { get; set; }
+		public string Address { get; set; }
+		public DateTime OrderDate { get; set; }
+		public decimal TotalPrice { get; set; }
+		public List<OrderItemResponse> OrderItems { get; set; }
+	}
+}
