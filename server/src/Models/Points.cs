ed63f28d9d3624a3760bd25d5b4a95bfcdce31c1
charlotte575757 <charlottepny@gmail.com>
2025-01-26 22:40:06 +0800

    This is the backend for the points system (for Jiwei's product)

---
+++ server/src/Models/Points.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Point 
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Range(1, 5)]
+    [Required]
+    public int Value { get; set; }
+
+    [MaxLength(500)] // You can limit the description length if needed
+    public string Description { get; set; }
+}
