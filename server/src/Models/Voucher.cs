ee0f3dbe3689c5c562dab0ecc487ddfeeab7659d
charlotte575757 <charlottepny@gmail.com>
2025-01-31 17:07:36 +0800

    Updated vouchersController and voucher for backend (i dont know why it is not updated and saved)

--- server/src/Models/Voucher.cs
+++ server/src/Models/Voucher.cs
@@ -4,8 +4,9 @@
     {
         public int VoucherId { get; set; }
         public string VoucherName { get; set; } = string.Empty;
-        public string VoucherDescription { get; set; } = string.Empty;
+        public Double DiscountPercentage { get; set; }
         public DateTime ExpiryDate { get; set; }
+
         public int TierId { get; set; } // Foreign key for Tier
         public Tier Tier { get; set; } // Navigation property
     }
