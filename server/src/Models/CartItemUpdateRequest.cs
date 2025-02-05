62f03b734004765c1f4ce438906c223ea9fa0a65
KenNuYu <tanjy0206@gmail.com>
2025-02-05 22:06:20 +0800

    feat: added functional cart with ui into navbar

---
+++ server/src/Models/CartItemUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace vegeatery.Models
+{
+	public class CartItemUpdateRequest
+	{
+		public int CartId { get; set; }
+		public int ProductId { get; set; }
+		public int Quantity { get; set; }
+	}
+}
