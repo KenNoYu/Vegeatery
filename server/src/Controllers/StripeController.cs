dc6a4835ab56370764aaed7dfdf316185a22347f
KenNuYu <tanjy0206@gmail.com>
2025-02-16 22:36:40 +0800

    feat: stripe controller

---
+++ server/src/Controllers/StripeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace vegeatery.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class StripeController : Controller
+	{
+		public StripeController()
+		{
+			StripeConfiguration.ApiKey = "your_stripe_secret_key";
+		}
+
+		[HttpGet("sales-summary")]
+		public IActionResult GetSalesSummary()
+		{
+			var service = new PaymentIntentService();
+			var paymentIntents = service.List(new PaymentIntentListOptions
+			{
+				Limit = 100
+			});
+
+			var totalSales = paymentIntents.Data.Sum(pi => pi.AmountReceived) / 100.0; // Convert from cents to dollars
+			var totalOrders = paymentIntents.Data.Count;
+
+			return Ok(new
+			{
+				TotalSales = totalSales,
+				TotalOrders = totalOrders
+			});
+		}
+	}
+}
