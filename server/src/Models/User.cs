0db892034ec33e39d3507af9e4265f8df9b0863a
charlotte575757 <charlottepny@gmail.com>
2025-02-16 20:35:15 +0800

    Fix: Naming of OrderPeriodStartDate

--- server/src/Models/User.cs
+++ server/src/Models/User.cs
@@ -64,7 +64,7 @@ public class User
 	// Navigation property
 	public Cart Cart { get; set; }
     public int OrderCount { get; set; } = 0;
-    public DateTime? OrderPeriodStart { get; set; }
+    public DateTime? OrderPeriodStartDate { get; set; } = null;
 
     // 🔹 Relationship: A User can have multiple feedbacks
     public virtual ICollection<GeneralFeedback>? Feedbacks { get; set; }
