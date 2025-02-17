7004efe03377d165d044846d7af2567c5a175a7f
AndrewAce <andrewkangmingyi@gmail.com>
2025-02-17 23:31:12 +0800

    feat: allow access to user reservations page for all user types

--- client/src/pages/Accounts/User/UserReservations.jsx
+++ client/src/pages/Accounts/User/UserReservations.jsx
@@ -14,7 +14,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const MyReservationsPage = () => {
-  RoleGuard('User');
+  RoleGuard(["User", "Admin", "Staff"]);
   const myTheme = useTheme();
   const navigate = useNavigate()
   const [user, setUser] = useState(null);
