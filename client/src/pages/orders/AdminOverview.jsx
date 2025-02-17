82871e2c8e5bebfe4d9070855656c24b1a5b1d52
KenNuYu <tanjy0206@gmail.com>
2025-02-17 12:55:46 +0800

    feat: top products feature

--- client/src/pages/orders/AdminOverview.jsx
+++ client/src/pages/orders/AdminOverview.jsx
@@ -19,6 +19,7 @@ const OrderDashboard = () => {
                 console.log(res.data);
                 setOrderData(res.data)
                 setSalesSummary(res.data)
+                getTopOrders();
             })
             .catch((err) => {
                 console.error('Failed to fetch order stats:', err)
@@ -32,7 +33,15 @@ const OrderDashboard = () => {
         { icon: <User />, label: 'Total Users', value: '' }
     ];
 
-    
+    const getTopOrders = () => {
+        http.get("/product/TopOrders")
+            .then((res) => {
+                setTopProducts(res.data)
+            })
+            .catch((err) => {
+                console.error('Failed to fetch order stats:', err)
+            });
+    }
 
     return (
         <Box p={3}>
@@ -40,7 +49,7 @@ const OrderDashboard = () => {
             <Grid container spacing={5}>
                 {/* Sales Chart */}
                 <Grid item xs={12} md={9} lg={9}>
-                    <Card sx={{ height: 350, width:{xs: '80vw', md: '60vw', lg: '45vw'} }}>
+                    <Card sx={{ height: 350, width: { xs: '80vw', md: '60vw', lg: '45vw' } }}>
                         <CardContent>
                             <Typography variant="h6" fontWeight="bold">Sales Summary</Typography>
                             <Box sx={{ width: '100%', height: 280 }}>
@@ -73,14 +82,20 @@ const OrderDashboard = () => {
             </Grid>
 
             {/* Top Selling Products */}
-            {/*
             <Card sx={{ mt: 3 }}>
                 <CardContent>
                     <Typography variant="h6" fontWeight="bold">Top Selling Products</Typography>
                     {topProducts.map(product => (
-                        <Box key={product.id} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2, bgcolor: '#f5f5f5', mb: 2, borderRadius: 2 }}>
+                        <Box key={product.productId} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2, bgcolor: '#f5f5f5', mb: 2, borderRadius: 2 }}>
+                            <Box sx={{ width: 60, height: 60, overflow: 'hidden', borderRadius: 2, mr: 2 }}>
+                                <img
+                                    src={`${import.meta.env.VITE_FILE_BASE_URL}${product.imageFile}`}
+                                    alt={product.productName}
+                                    style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }}
+                                />
+                            </Box>
                             <Box>
-                                <Typography fontWeight="medium" variant="subtitle1">{product.name}</Typography>
+                                <Typography fontWeight="medium" variant="subtitle1">{product.productName}</Typography>
                                 <Box sx={{ display: 'flex', gap: 1, mt: 0.5 }}>
                                     {product.tags.map(tag => (
                                         <Badge key={tag} sx={{ p: 0.5, bgcolor: '#e0e0e0', borderRadius: 1 }}>{tag}</Badge>
@@ -88,14 +103,13 @@ const OrderDashboard = () => {
                                 </Box>
                             </Box>
                             <Box textAlign="right">
-                                <Typography color="error" fontWeight="bold">Earnings: {product.earnings}</Typography>
-                                <Typography variant="caption" color="textSecondary">x {product.orders}</Typography>
+                                <Typography color="error" fontWeight="bold">Earnings: ${product.earnings * product.quantityBought}</Typography>
+                                <Typography variant="caption" color="textSecondary">x{product.quantityBought} Sold</Typography>
                             </Box>
                         </Box>
                     ))}
                 </CardContent>
             </Card>
-            */}
         </Box>
     )
 };
