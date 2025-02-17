7fba69761a153502575a56cfa64aadcb35f61ec5
charlotte575757 <charlottepny@gmail.com>
2025-02-17 18:22:17 +0800

    Feat: Create Logs for orders' points record and bonus points record

--- server/src/Controllers/PointsHistoryController.cs
+++ server/src/Controllers/PointsHistoryController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using vegeatery.Models;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace vegeatery.Controllers
 {
@@ -20,13 +21,16 @@ namespace vegeatery.Controllers
         [HttpGet("{customerId}")]
         public async Task<IActionResult> GetPointsHistory(int customerId)
         {
+            var sgTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Singapore Standard Time");
+
             var orders = await _context.Order
                 .Where(o => o.CustomerId == customerId)
-                .OrderByDescending(o => o.OrderDate)
+                .OrderByDescending(o => o.CreatedAt)
                 .Select(o => new
                 {
                     o.OrderId,
                     o.OrderDate,
+                    CreatedAt = TimeZoneInfo.ConvertTimeFromUtc(o.CreatedAt, sgTimeZone),
                     o.TotalPoints
                 })
                 .ToListAsync();
@@ -37,6 +41,30 @@ namespace vegeatery.Controllers
             }
 
             return Ok(orders);
+        }
+
+        [HttpGet("bonus/{userId}")]
+        public IActionResult GetBonusPointsHistory(int userId)
+        {
+            var sgTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Singapore Standard Time");
+
+            var bonusPoints = _context.PointsHistories
+                .Where(ph => ph.UserId == userId)
+                .OrderByDescending(ph => ph.Date)
+                .Select(ph => new
+                {
+                    ph.Points,
+                    ph.Description,
+                    Date = TimeZoneInfo.ConvertTimeFromUtc(ph.Date, sgTimeZone) // Convert to SGT
+                })
+                .ToList();
+
+            if (!bonusPoints.Any())
+            {
+                return NotFound("No bonus points found for this user.");
+            }
+
+            return Ok(bonusPoints);
         }
     }
 }
