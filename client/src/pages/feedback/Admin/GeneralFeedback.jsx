49be64dca25c84e9019422e4db44e7995fccd0ab
charlotte575757 <charlottepny@gmail.com>
2025-02-18 03:28:12 +0800

    Fix: Fix styling of list and paper

--- client/src/pages/feedback/Admin/GeneralFeedback.jsx
+++ client/src/pages/feedback/Admin/GeneralFeedback.jsx
@@ -106,96 +106,97 @@ const AdminGeneralFeedback = () => {
         <FeedbackSidebar />
 
         {/* Main Content */}
-        <Box
-          sx={{
-            marginLeft: "240px",
-            flexGrow: 1,
-            width: "80%",
-            padding: 5,
-            backgroundColor: "#FFFFFF",
-            marginTop: "5px",
-            paddingLeft: "3em",
-            overflowX: "hidden",
-          }}
-        >
+          <Box
+            sx={{
+              marginLeft: "240px",
+              flexGrow: 1,
+              width: "80%",
+              padding: 5,
+              backgroundColor: "#FFFFFF",
+              marginTop: "5px",
+              paddingLeft: "3em",
+              overflowX: "hidden",
+            }}
+          >
 
-          <Typography variant="h4" fontWeight="bold" mt={5} ml={3} mb={2}>Customer Feedback</Typography>
-          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-          <List sx={{ display: "flex", flexDirection: "column", gap: 2, width: '100%' }}>
-            {feedbackList.map((feedback) => (
-              <Paper
-                key={feedback.id}
-                elevation={3}
-                sx={{
-                  padding: 2,
-                  borderRadius: 2,
-                  backgroundColor: "#f9f9f9",
-                  border: "1px solid #ddd",
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: 2,
-                  marginLeft: 3,
+            <Typography variant="h4" fontWeight="bold" mt={5} ml={3} mb={2}>Customer Feedback</Typography>
+            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+            <List sx={{ display: "start", flexDirection: "column", gap: 2, width: 850 }}>
+              {feedbackList.map((feedback) => (
+                <Paper
+                  key={feedback.id}
+                  elevation={3}
+                  sx={{
+                    padding: 2,
+                    borderRadius: 2,
+                    backgroundColor: "#f9f9f9",
+                    border: "1px solid #ddd",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 2,
+                    marginLeft: 0,
+                    marginRight: 3
 
-                }}
-              >
-                <ListItem disablePadding>
-                  <ListItemText
-                    primary={`Title: ${feedback.feedbackTitle}`}
-                    secondary={
-                      <>
-                        <Box marginBottom={2} marginTop={2}>
-                          {feedback.imagePath ? (
-                            <img
-                              src={feedback.imagePath}
-                              alt="Feedback Image"
-                              style={{
-                                width: "100px",
-                                height: "100px",
-                                objectFit: "cover",
-                              }}
-                            />
-                          ) : (
-                            <Typography>No image provided</Typography>
-                          )}
-                        </Box>
+                  }}
+                >
+                  <ListItem disablePadding>
+                    <ListItemText
+                      primary={`Title: ${feedback.feedbackTitle}`}
+                      secondary={
+                        <>
+                          <Box marginBottom={2} marginTop={2}>
+                            {feedback.imagePath ? (
+                              <img
+                                src={feedback.imagePath}
+                                alt="Feedback Image"
+                                style={{
+                                  width: "100px",
+                                  height: "100px",
+                                  objectFit: "cover",
+                                }}
+                              />
+                            ) : (
+                              <Typography>No image provided</Typography>
+                            )}
+                          </Box>
 
-                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                          <Typography variant="body1" sx={{ fontWeight: 'bold' }}>Rating:</Typography>
-                          <Box>{renderStars(feedback.rating)}</Box>
-                        </Box>
-                        <Box sx={{ whiteSpace: 'normal', wordWrap: 'break-word', marginTop: 1, marginBottom: 1 }}>
-                          <Typography variant="body2" sx={{ fontWeight: 'bold' }}>User's Email: {feedback.email}</Typography>
-                        </Box>
-                        <Box sx={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
-                          <Typography variant="body1">{feedback.review}</Typography>
-                        </Box>
+                          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                            <Typography variant="body1" sx={{ fontWeight: 'bold' }}>Rating:</Typography>
+                            <Box>{renderStars(feedback.rating)}</Box>
+                          </Box>
+                          <Box sx={{ whiteSpace: 'normal', wordWrap: 'break-word', marginTop: 1, marginBottom: 1 }}>
+                            <Typography variant="body2" sx={{ fontWeight: 'bold' }}>User's Email: {feedback.email}</Typography>
+                          </Box>
+                          <Box sx={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>
+                            <Typography variant="body1">{feedback.review}</Typography>
+                          </Box>
 
 
-                      </>
-                    }
-                    sx={{ marginRight: 2 }}
-                  />
-                  <Button
-                    variant="outlined"
-                    color="black"
-                    onClick={() => handleWriteToUser(feedback)}
-                    sx={{
-                      minWidth: '150px',
-                      alignSelf: 'flex-start',
-                      textTransform: 'none',
-                      color: '#FFFFFF',
-                      backgroundColor: '#C6487E',
-                      '&:hover': { backgroundColor: '#E7ABC5' }
+                        </>
+                      }
+                      sx={{ marginRight: 2 }}
+                    />
+                    <Button
+                      variant="outlined"
+                      color="black"
+                      onClick={() => handleWriteToUser(feedback)}
+                      sx={{
+                        minWidth: '150px',
+                        alignSelf: 'flex-start',
+                        textTransform: 'none',
+                        color: '#FFFFFF',
+                        backgroundColor: '#C6487E',
+                        '&:hover': { backgroundColor: '#E7ABC5' }
 
-                    }}
-                  >
-                    WRITE TO USER
-                  </Button>
-                </ListItem>
-              </Paper>
-            ))}
-          </List>
-        </Box>
+                      }}
+                    >
+                      WRITE TO USER
+                    </Button>
+                  </ListItem>
+                </Paper>
+              ))}
+            </List>
+          </Box>
 
         {/* Reply Modal */}
         <Dialog open={openModal} onClose={() => setOpenModal(false)}>
